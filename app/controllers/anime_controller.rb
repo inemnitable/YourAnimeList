fbad66c1b495a8fccefddd5d63709a58f8ac1548
App Academy Student <student@appacademy.io>
2013-09-09 10:18:19 -0700

    fix searching for users with an empty list

--- app/controllers/anime_controller.rb
+++ app/controllers/anime_controller.rb
@@ -18,11 +18,15 @@ class AnimeController < ApplicationController
 
   def search
     search_str = params[:search]
-    @anime = Anime.where(
-      'LOWER(title) LIKE ? AND id NOT IN (?)',
-       "%#{search_str.downcase}%",
-       current_user.anime_ids)
-      .limit(15)
+    anime_ids = current_user.anime_ids
+    @anime = anime_ids.count > 0 ?
+      Anime.where("LOWER(title) LIKE ? AND id NOT IN (?)",
+        "%#{search_str.downcase}%",
+        current_user.anime_ids)
+        .limit(15) :
+      Anime.where("LOWER(title) LIKE ?",
+        "%#{search_str.downcase}%")
+        .limit(15)
     render :search
   end
 end
